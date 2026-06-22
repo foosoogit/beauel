99bb90c2e52ac48f7a6023c5549eaa38ef705d6b
foosoogit <awa@szemi-gp.com>
2026-06-22 11:47:00 +0900

    routeのname修正-3

--- project/resources/views/customers/CreateCustomer.blade.php
+++ project/resources/views/customers/CreateCustomer.blade.php
@@ -19,7 +19,7 @@ input,textarea{
 			@if(session('target_branch_name')=="")
 				<p class="display-1 text-danger">店舗が選択されていません。</p>
 			@endif
-				<form name='input_customer_fm' id="input_customer_fm" action="{{ route('customers.insertCustomer')}}" method="POST" class="h-adr">@csrf
+				<form name='input_customer_fm' id="input_customer_fm" action="{{ route('customers.insertCustomer.post')}}" method="POST" class="h-adr">@csrf
 					@if (optional($target_user)->serial_user<>null)
 						<p><div class="mb-2 bg-secondary text-white"><span class="border border-success bg-success text-white rounded h5">&nbsp;{{session('target_branch_name')}}&nbsp;</span>顧客データ修正</div></p>
 						<p>顧客番号<input type="text" name="serial_user" value="{{optional($target_user)->serial_user}}" class="bg-white-500 border-solid pxtext-black rounded px-3 py-1" readonly></p>
