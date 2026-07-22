6e88a0cc425479a8d0102452dc0e6bb95c36dc4e
foosoogit <awa@szemi-gp.com>
2026-07-22 18:15:54 +0900

    cashbookのnotfound処理-2

--- project/config/app.php
+++ project/config/app.php
@@ -171,6 +171,7 @@ return [
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
         SimpleSoftwareIO\QrCode\QrCodeServiceProvider::class,
+        Livewire\LivewireServiceProvider::class,
     ])->toArray(),
 
     /*
